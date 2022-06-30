2243a36d894380ff366d76011ae60c29aa6458d5
BrianJenney <brianjenney83@gmail.com>
2022-06-30 09:25:50 -0700

    used named exports

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-export { default as LoginButton } from './LoginButton';
-export { default as LogoutButton } from './LogoutButton';
-export { default as Loader } from './Loader';
+export { LoginButton } from './LoginButton';
+export { LogoutButton } from './LogoutButton';
+export { Loader } from './Loader';
 export { StyleConfigForm } from './StyleConfigForm';
 export { NavBar } from './NavBar';
 export { SubscriptionCard } from './SubscriptionCard';
